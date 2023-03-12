438083e512233a82e5388021603732b1ee2c2f8e
Nick White <nick@nickpale.com>
2023-03-12 13:42:17 -0700

    Add message to getAllItems body

--- src/handlers/get-all-items.mjs
+++ src/handlers/get-all-items.mjs
@@ -35,7 +35,10 @@ export const getAllItemsHandler = async (event) => {
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: JSON.stringify({
+            items,
+            message: 'Yo!'
+        })
     };
 
     // All log statements are written to CloudWatch
