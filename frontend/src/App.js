e35e186a7902f286d7efc3e5216811ddfb199978
Nick White <nick@nickpale.com>
2023-05-15 11:16:17 -0700

    Add link to login page
    This commit includes environment variables in the .env files.
    Read dotenv documentation for more details on usage.
    
    Also adding localhost to dev auth redirect options

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import {loginURL} from './urls';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,16 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={loginURL.href}
         >
-          Learn React
+          Log in
         </a>
       </header>
     </div>
---
+++ frontend/src/urls.js
@@ -0,0 +1,12 @@
+export const baseURL = new URL("/", process.env.REACT_APP_BASE_URL);
+
+const authURL = new URL("/", process.env.REACT_APP_AUTH_URL);
+
+export const loginURL = (() => {
+  const loginURL = new URL('/login', authURL);
+  loginURL.searchParams.set('client_id', process.env.REACT_APP_COGNITO_CLIENT_ID);
+  loginURL.searchParams.set('response_type', 'code');
+  loginURL.searchParams.set('scope', 'email+openid+phone+profile');
+  loginURL.searchParams.set('redirect_uri', encodeURIComponent(baseURL.href));
+  return loginURL;
+})();
