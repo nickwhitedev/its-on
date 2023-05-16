b13e6d553f543c43c3a8e1b5e0963d6c03b6ca55
Nick White <nick@nickpale.com>
2023-05-15 20:28:47 -0700

    Connect the dots on action outputs

--- frontend/scripts/cicd-set-env-vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Used to set environment variables during github actions
-# Meant to be run from the frontend directory
-echo "REACT_APP_COGNITO_CLIENT_ID=\"${{needs.build-and-deploy-feature-api.outputs.CognitoClientID}}\"" >> .env.production
-echo "REACT_APP_BASE_URL=\"${{needs.build-and-deploy-feature-api.outputs.WebEndpoint}}\"" >> .env.production
-echo "REACT_APP_AUTH_URL=\"${{needs.build-and-deploy-feature-api.outputs.AuthEndpoint}}\"" >> .env.production
-echo "REACT_APP_API_URL=\"${{needs.build-and-deploy-feature-api.outputs.ApiEndpoint}}\"" >> .env.production
