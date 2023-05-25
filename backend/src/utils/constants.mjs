3e8a8ebd329adb8b733a5e5c44b47dfbd27f72c4
Nick White <nick@nickpale.com>
2023-05-25 08:57:24 -0700

    Add Authorization to CORS headers

--- backend/src/utils/constants.mjs
+++ backend/src/utils/constants.mjs
@@ -1,5 +1,5 @@
 export const CORS_HEADERS = {
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "*, Authorization",
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
 };
