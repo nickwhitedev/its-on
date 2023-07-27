c33542e482750df29e7884e39ba260f54736f3f4
Nick White <nick@nickpale.com>
2023-07-26 22:01:25 -0700

    Add router to unit test

--- frontend/src/components/__tests__/App.test.js
+++ frontend/src/components/__tests__/App.test.js
@@ -1,10 +1,17 @@
 import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import App from '../App.react'
 
 jest.mock('../channels/Channels.react')
 
 test('renders learn react link', () => {
-  render(<App />)
+  // TODO: Add helper renderWithRouter function
+  // https://testing-library.com/docs/example-react-router/#reducing-boilerplate
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  )
   const linkElement = screen.getByText(/Logout/i)
   expect(linkElement).toBeInTheDocument()
 })
