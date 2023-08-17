329af0095c9192c70154d8881f9fd015f3f517d8
Nick White <nick@nickpale.com>
2023-08-17 14:04:02 -0400

    Add update api call to activate channel button

--- frontend/src/components/channels/channel/Channel.tsx
+++ frontend/src/components/channels/channel/Channel.tsx
@@ -1,11 +1,38 @@
+import { useChannelsDispatch } from "../../../contexts/channels/channelsContext"
+import { ChannelsDispatchActionType } from "../../../contexts/channels/channelsReducer"
+import { fetchApi } from "../../../utils/api"
+
 interface Props {
   channel: IChannel,
 }
 
 const Channel = ({ channel }: Props) => {
+  const dispatch = useChannelsDispatch()
+
+  const handleClickItsOn = async () => {
+    try {
+      const newChannel: IChannel = await fetchApi(`/channels/${channel.id}`, 'PUT', {
+        compositeId: channel.compositeId,
+        defaultNote: channel.defaultNote,
+        id: channel.id,
+        note: channel.note,
+        on: !channel.on,
+        title: channel.title,
+      })
+      dispatch({
+        type: ChannelsDispatchActionType.CHANGED,
+        channel: newChannel
+      })
+    } catch (error) {
+      // TODO: Handle create channel error
+      // log error to backend
+      // display user friendly message
+    }
+  }
+
   return (
     <div>
-      <button>Activate/Deactivate</button>
+      <button onClick={() => void handleClickItsOn()}>Activate/Deactivate</button>
       <h2>{channel.title}</h2>
       <p>{channel.note || channel.defaultNote}</p>
     </div>
