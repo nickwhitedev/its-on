6eb2f6f0f98f518ce31814c1b5079d969ae5afcb
Nick White <nick@nickpale.com>
2023-08-17 20:01:33 -0400

    List subscriptions on subscriptions page

--- frontend/src/components/subscriptions/Subscriptions.tsx
+++ frontend/src/components/subscriptions/Subscriptions.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
+import { useSubscriptions } from "../../contexts/subscriptions/subscriptionsContext"
+
 const Subscriptions = () => {
+  const subscriptions = useSubscriptions()
+
   return (
     <div>
       <h2>Subscriptions</h2>
+      {subscriptions.map((channel, index) => (
+        <div key={index}>
+          <Link to={`/channels/${channel.id}`}>{channel.title}</Link> - {' '}
+          {channel.owner} - {channel.on ? 'on' : 'off'} - {channel.note || channel.defaultNote}
+        </div>
+      ))}
     </div>
   );
 }
