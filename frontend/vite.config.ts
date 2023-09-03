29c5f6c6fe4275ac3d9ec791fe6f5ae759020b3d
Nick White <nick@nickpale.com>
2023-09-02 19:22:03 -0700

    Add minimal-ui display config and fix header color

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         name: "It's On",
         short_name: 'ItsOn',
         description: "Always know when it's on",
+        display: 'minimal-ui',
         start_url: '/',
         theme_color: '#0e1118',
         background_color: '#0e1118',
