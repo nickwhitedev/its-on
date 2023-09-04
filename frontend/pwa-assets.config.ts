2a1f567c8d370b734b807333697fbb0de27fb88a
Nick White <nick@nickpale.com>
2023-09-04 11:47:36 -0700

    Update icons

--- frontend/pwa-assets.config.ts
+++ frontend/pwa-assets.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
   images: ['public/its-on.svg'],
+  // images: ['public/favicon-light.svg'], // Used to generate favicon.ico
   preset: {
     transparent: {
       sizes: [64, 192, 512],
+      // favicons: [[64, 'favicon.ico']],
     },
     maskable: {
       sizes: [512],
