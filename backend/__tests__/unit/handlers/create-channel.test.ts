3170d985bdd9c20d3eaf2e591b9efb52d7f1ce92
Nick White <nick@nickpale.com>
2023-09-12 23:07:02 -0700

    Add character limits to title and note
    This commit also adds title back to the update lambda.

--- backend/__tests__/unit/handlers/create-channel.test.ts
+++ backend/__tests__/unit/handlers/create-channel.test.ts
@@ -4,12 +4,12 @@ import {
   PutCommand,
 } from '@aws-sdk/lib-dynamodb'
 
-import { jest } from '@jest/globals'
 import { APIGatewayProxyEvent } from 'aws-lambda'
+import { CORS_HEADERS } from '../../../src/utils/constants'
+import { createChannelHandler } from '../../../src/handlers/create-channel'
+import { jest } from '@jest/globals'
 import { mockClient } from 'aws-sdk-client-mock'
 import mockEvent from '../../../__mocks__/mock-event'
-import { createChannelHandler } from '../../../src/handlers/create-channel'
-import { CORS_HEADERS } from '../../../src/utils/constants'
 
 // This includes all tests for createChannelHandler()
 describe('Test createChannelHandler', function () {
@@ -54,6 +54,41 @@ describe('Test createChannelHandler', function () {
     expect(resultBody.on).toEqual(false)
     expect(resultBody.owner).toEqual('test_user')
     expect(resultBody.title).toEqual('Super Channel')
+  })
+
+  it('should trim long title', async () => {
+    // Return the specified value whenever the spied put function is called
+    ddbMock.on(GetCommand).resolves({})
+    ddbMock.on(PutCommand).resolves({})
+
+    const testLongTitle = 'Super Channel'.repeat(100)
+
+    const event: APIGatewayProxyEvent = {
+      ...mockEvent,
+      body: `{"title": "${testLongTitle}"}`,
+      httpMethod: 'POST',
+    }
+
+    // Invoke createChannelHandler()
+    const result = await createChannelHandler(event)
+
+    const resultBody = JSON.parse(result.body) as IChannel
+    // Compare the result with the expected result
+    expect(result.headers).toEqual(CORS_HEADERS)
+    expect(result.statusCode).toEqual(201)
+
+    expect(Object.keys(resultBody)).toEqual([
+      'id',
+      'note',
+      'on',
+      'owner',
+      'title',
+    ])
+    expect(resultBody.note).toEqual('')
+    expect(resultBody.on).toEqual(false)
+    expect(resultBody.owner).toEqual('test_user')
+    expect(resultBody.title).toHaveLength(40)
+    expect(resultBody.title).toEqual(testLongTitle.substring(0, 40))
   })
 
   it('should retry nanoid when there is an id collision', async () => {
