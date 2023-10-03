7597f9f1d9c47b2c09e017c368cc454c7fddcd87
Nick White <nick@nickpale.com>
2023-10-03 12:34:55 -0700

    Add material design list for channel lists

--- frontend/src/components/channels/Channels.tsx
+++ frontend/src/components/channels/Channels.tsx
@@ -1,24 +1,50 @@
-import { Link } from 'react-router-dom'
+import './Channels.css'
+
+import { useNavigate } from 'react-router-dom'
 import { useChannels } from '../../contexts/channels/channelsContext'
+import ItsOnIcon from '../icons/ItsOnIcon'
+import MDIcon from '../material/MDIcon'
+import MDList from '../material/MDList'
+import MDListItem from '../material/MDListItem'
 import CreateChannel from './CreateChannel'
 import { isChannelOn } from './channel/channelUtils'
 
 const Channels = () => {
   const channels = useChannels()
+  const navigate = useNavigate()
 
   return (
-    <div>
-      <h2>Channels</h2>
+    <div className={'Channels'}>
       <CreateChannel />
-      {channels.map((channel, index) => (
-        <div key={index}>
-          <Link to={`/${channel.id}`}>
-            {(channel.title?.length ?? 0) > 0 ? channel.title : 'Untitled'}
-          </Link>{' '}
-          - {isChannelOn(channel) ? 'on' : 'off'}
-          {(channel.note?.length ?? 0) > 0 ? ` - ${channel.note}` : ''}
-        </div>
-      ))}
+      <MDList>
+        {channels.map((channel, index) => (
+          <MDListItem
+            className={'Channels-list-item'}
+            key={index}
+            type='link'
+            onClick={() => {
+              navigate(`/${channel.id}`)
+            }}
+          >
+            <MDIcon
+              className={
+                isChannelOn(channel)
+                  ? 'Channels-list-item-on'
+                  : 'Channels-list-item-off'
+              }
+              slot='start'
+            >
+              <ItsOnIcon />
+            </MDIcon>
+            <div slot='headline'>
+              {(channel.title?.length ?? 0) > 0 ? channel.title : 'Untitled'}
+            </div>
+            {(channel.note?.length ?? 0) > 0 ? (
+              <div slot='supporting-text'>{channel.note}</div>
+            ) : null}
+          </MDListItem>
+        ))}
+      </MDList>
     </div>
   )
 }
