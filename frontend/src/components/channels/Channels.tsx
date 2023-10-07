3bc78be52d9adfce5bfd434caf7f862ca5dd724f
Nick White <nick@nickpale.com>
2023-10-07 15:35:04 -0700

    Add create channel item to channels list

--- frontend/src/components/channels/Channels.tsx
+++ frontend/src/components/channels/Channels.tsx
@@ -1,29 +1,66 @@
 import './Channels.css'
 
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useChannels } from '../../contexts/channels/channelsContext'
+import {
+  useChannels,
+  useChannelsDispatch,
+} from '../../contexts/channels/channelsContext'
+import { ChannelsDispatchActionType } from '../../contexts/channels/channelsReducer'
+import { fetchApi } from '../../utils/api'
 import ItsOnIcon from '../icons/ItsOnIcon'
 import MDDivider from '../material/MDDivider'
 import MDIcon from '../material/MDIcon'
 import MDList from '../material/MDList'
 import MDListItem from '../material/MDListItem'
-import CreateChannel from './CreateChannel'
 import { isChannelOn } from './channel/channelUtils'
 
 const Channels = () => {
   const channels = useChannels()
+  const dispatch = useChannelsDispatch()
+
   const navigate = useNavigate()
+
+  const [isCreating, setIsCreating] = useState<boolean>(false)
+
+  const handleCreateChannel = async () => {
+    setIsCreating(true)
+
+    try {
+      const newChannel: IChannel = await fetchApi('/channels', 'POST', {
+        title: '',
+      })
+      dispatch({
+        type: ChannelsDispatchActionType.ADDED,
+        channel: newChannel,
+      })
+      setIsCreating(false)
+      navigate(`/${newChannel.id}`)
+    } catch (error) {
+      // TODO: Handle create channel error
+      // log error to backend
+      // display user friendly message
+    }
+    setIsCreating(false)
+  }
 
   return (
     <div className={'Channels'}>
-      <CreateChannel />
       <MDList>
-        {channels.map((channel, index) => (
+        <MDListItem
+          className={'Channels-list-item'}
+          disabled={isCreating}
+          type='button'
+          onClick={() => void handleCreateChannel()}
+        >
+          <MDIcon slot='start'>add</MDIcon>
+          <div slot='headline'>Create Channel</div>
+        </MDListItem>
+        {channels.map(channel => (
           <>
-            {index > 0 ? <MDDivider></MDDivider> : null}
+            <MDDivider></MDDivider>
             <MDListItem
               className={'Channels-list-item'}
-              key={index}
               type='link'
               onClick={() => {
                 navigate(`/${channel.id}`)
