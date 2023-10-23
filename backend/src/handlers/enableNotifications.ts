c57c52e9e58d8c78cb9560f2b6133d11c5b173dd
Nick White <nick@nickpale.com>
2023-10-23 09:53:46 -0700

    Add enable notifications endpoint
    Saves the logged in user's notification subscription
    to their profile in DynamoDB.

---
+++ backend/src/handlers/enableNotifications.ts
@@ -0,0 +1,74 @@
+import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DYNAMODB_TABLE_NAME } from '../utils/constants'
+import { createResponse } from '../utils/response'
+
+const client = new DynamoDBClient({})
+const ddbDocClient = DynamoDBDocumentClient.from(client)
+
+interface IPayload {
+  notificationSubscription: string
+}
+
+/**
+ * Adds a user's notification subscription to their profile in Dynamo DB
+ */
+export const enableNotificationsHandler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  if (event.httpMethod !== 'POST') {
+    throw new Error(
+      `postMethod only accepts POST method, you tried: ${event.httpMethod} method.`,
+    )
+  }
+  console.debug('received:', event)
+
+  const eventPath = event.path
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const userID: string = event.requestContext.authorizer?.claims?.sub ?? ''
+
+  try {
+    const ddbResponse = await ddbDocClient.send(
+      new UpdateCommand({
+        Key: {
+          pk: `user#${userID}`,
+          sk: `profile`,
+        },
+        ReturnValues: 'ALL_NEW',
+        TableName: DYNAMODB_TABLE_NAME,
+        UpdateExpression:
+          'SET #notificationSubscription = :notificationSubscription',
+        ExpressionAttributeNames: {
+          '#notificationSubscription': 'notificationSubscription',
+        },
+        ExpressionAttributeValues: {
+          ':notificationSubscription': (
+            JSON.parse(
+              event.body ?? JSON.stringify({ subscription: '' }),
+            ) as IPayload
+          ).notificationSubscription,
+        },
+      }),
+    )
+    console.info('Success - user profile updated', ddbResponse)
+  } catch (error) {
+    console.error(
+      'Error',
+      error instanceof Error ? error.stack : 'Unknown Type',
+    )
+    return createResponse({
+      eventPath,
+      responseBody: { message: 'Something went wrong' },
+      statusCode: 400,
+    })
+  }
+
+  return createResponse({
+    eventPath,
+    responseBody: { message: 'Notifications Enabled' },
+    statusCode: 200,
+  })
+}
