ceabfcb1b2ded4f9acd36ebb626c4ee25a721c22
Nick White <nick@nickpale.com>
2023-11-01 23:12:25 -0700

    Test without icon

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -90,10 +90,10 @@ self.addEventListener('push', function (event) {
 
   event.waitUntil(
     self.registration.showNotification(data.title, {
-      // badge: 'badge-128x128.png',
+      // badge: '/badge-128x128.png',
       body: data.options.body,
       data: data.options.data,
-      icon: 'pwa-512x512.png',
+      // icon: '/pwa-512x512.png',
     }),
   )
 })
