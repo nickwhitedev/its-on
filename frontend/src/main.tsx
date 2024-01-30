a35b89136ee8d6bb07bebd708580fff8124642e0
Nick White <nick@nickpale.com>
2024-01-29 21:28:19 -0800

    Add clerk dependency

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,27 @@
 import './index.css'
 
-import { Authenticator } from '@aws-amplify/ui-react'
+import { ClerkProvider } from '@clerk/clerk-react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import AppContextProvider from './contexts/AppContextProvider'
 import router from './routes/router'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 const rootElement = document.getElementById('root')
 
 if (rootElement != null) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <Authenticator.Provider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <AppContextProvider>
           <RouterProvider router={router} />
         </AppContextProvider>
-      </Authenticator.Provider>
+      </ClerkProvider>
     </React.StrictMode>,
   )
 }
