dd295e1467f6c63482d61cb27bd2d4e8110eede2
Nick White <nick@nickpale.com>
2024-02-22 23:30:43 -0800

    Add privacy policy and terms of service
    This commit also makes some Clerk appearance customizations.- Theme
    matching system theme
    - Primary color to match app accent color
    - Show terms and privacy links on clerk components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import './index.css'
 
 import { ClerkProvider } from '@clerk/clerk-react'
+import { dark } from '@clerk/themes'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import AppContextProvider from './contexts/AppContextProvider'
 import router from './routes/router'
+import { baseUrl } from './utils/urls'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string
 
@@ -17,7 +19,21 @@ const rootElement = document.getElementById('root')
 if (rootElement != null) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ClerkProvider
+        publishableKey={PUBLISHABLE_KEY}
+        appearance={{
+          baseTheme: window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? dark
+            : undefined,
+          layout: {
+            termsPageUrl: `${baseUrl}/terms`,
+            privacyPageUrl: `${baseUrl}/privacy`,
+          },
+          variables: {
+            colorPrimary: '#17c1e8',
+          },
+        }}
+      >
         <AppContextProvider>
           <RouterProvider router={router} />
         </AppContextProvider>
