55754102fee65b7175c5756f0a7df9781e349314
Nick White <nick@nickpale.com>
2024-02-25 17:17:16 -0800

    Update sw

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -95,10 +95,10 @@ self.addEventListener('push', function (event) {
 
   event.waitUntil(
     self.registration.showNotification(data.title, {
-      badge: './maskable-icon-192x192.png',
-      body: data.options.body,
+      badge: './monochrome-icon-192.png',
+      body: `test ${data.options.body}`,
       data: data.options.data,
-      icon: './maskable-icon-192x192.png',
+      icon: './monochrome-icon-192.png',
     }),
   )
 })
