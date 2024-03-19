ed1167ce25d8c695700ae5988e1ae60329e03e3f
Nick White <nick@nickpale.com>
2024-03-18 19:03:22 -0700

    Add support page

---
+++ frontend/src/components/support/Support.tsx
@@ -0,0 +1,21 @@
+const Support = () => {
+  return (
+    <div className='Support'>
+      <h3>Support</h3>
+      <p>
+        If you need help using It&apos;s On, send a message to at{' '}
+        <a href='mailto:help@itson.fyi'>help@itson.fyi</a>
+      </p>
+      <p>
+        To leave feedback, send an email to{' '}
+        <a href='mailto:feedback@itson.fyi'>feedback@itson.fyi</a>
+      </p>
+      <p>
+        For any other questions or comments, feel free to contact us at{' '}
+        <a href='mailto:contact@itson.fyi'>contact@itson.fyi</a>
+      </p>
+    </div>
+  )
+}
+
+export default Support
