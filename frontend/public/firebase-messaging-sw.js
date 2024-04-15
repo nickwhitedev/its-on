473a871ba4c23349cb9ffb76af6e30e9db7ec51d
Nick White <nick@nickpale.com>
2024-04-15 01:25:57 -0700

    Updgrade firebase imports in firebase-messaging-sw

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,9 +1,11 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js')
 importScripts(
-  'https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js',
+  'https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js',
+)
+importScripts(
+  'https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js',
 )
 
 // Initialize the Firebase app in the service worker by passing in
