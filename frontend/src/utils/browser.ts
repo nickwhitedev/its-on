91f41416e6cf4a3154d239c6bf4ca2cb5474bc13
Nick White <nick@itson.fyi>
2024-04-22 12:52:31 -0700

    Added credit to browser.ts

--- frontend/src/utils/browser.ts
+++ frontend/src/utils/browser.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react'
 
 export const useGetBrowserDisplayName = (): (() => Promise<string>) => {
+  // Taken from https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator
   // The order matters here, and this may report false positives for unlisted browsers.
   const userAgent = navigator.userAgent
   return useCallback(async (): Promise<string> => {
