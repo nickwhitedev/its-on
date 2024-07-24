4ec0bc305910f1c073cc102dec4536a76ac9d5f0
Nick White <nick@itson.fyi>
2024-07-23 19:30:33 -0700

    Update about page with more sections

--- frontend/src/components/about/About.tsx
+++ frontend/src/components/about/About.tsx
@@ -3,25 +3,53 @@ import './About.css'
 const About = () => {
   return (
     <div className='About'>
-      <h3>About It&apos;s On</h3>
+      <h3>About</h3>
+      <p>Made to be small.</p>
+      <p>Small channels.</p>
+      <p>Less notifications.</p>
+      <p>Less to manage.</p>
       <p>
-        It&apos;s On was made to be small. Small channels. Less notifications.
-        Less to manage. An easy app to use that only sends you notifications
-        that you care about.
+        An easy app to use that only sends you notifications that you care
+        about.
+      </p>
+      <h3>How to Use</h3>
+      <p>Create a channel.</p>
+      <p>Share your channel with your friends.</p>
+      <p>
+        Press the <strong>on</strong> button to let your friends know that
+        it&apos;s on!
+      </p>
+      <p>Subscribe to your friends&apos; channels to stay in the loop!</p>
+      <h3>Tips</h3>
+      <p>
+        Keep your channels and subscriptions focused on what you care about.
+      </p>
+      <p>Unsubscribe from channels that are inactive or overactive.</p>
+      <p>Delete channels you don&apos;t use anymore.</p>
+      <p>If you are being spammed, unsubscribe.</p>
+      <h3>Frequently Asked Questions</h3>
+      <p>
+        <strong>What happens when I press the on button?</strong>
       </p>
       <p>
-        Remember to keep your channels and subscriptions focused on what you
-        care about. Don&apos;t be afraid to delete channels you don&apos;t use
-        anymore or unsubscribe from channels that are inactive or overactive.
+        All of the subscribers of the channel get sent a notification with the
+        title and note on your channel.
       </p>
       <p>
-        If you are being spammed, know that you are in control and can simply
-        unsubscribe!
+        <strong>Why can I only have five subscribers on my channels?</strong>
       </p>
-      <p>You have the power to make this app your own, so own it!</p>
       <p>
-        <strong>It&apos;s On!</strong>
+        This app is focused on being a focused tool for small manageable groups,
+        so the free tier only allows five subscribers, five channels, and five
+        subscriptions. It is possible to upgrade your limits for free by using
+        the app regularly or you can pay for upgrades. The free tier is what
+        It&apos;s On was originally built for, so don&apos;t forget about it
+        once you increase your limits!
       </p>
+      <p>
+        <strong>Can I Delete My Channels?</strong>
+      </p>
+      <p>Yes! Keep things fresh and make new channels often!</p>
     </div>
   )
 }
