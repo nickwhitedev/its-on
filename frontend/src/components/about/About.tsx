b702bbdc13683212443d8eb08a492ce6856059db
Nick White <nick@itson.fyi>
2024-07-23 20:07:38 -0700

    Add channel examples

--- frontend/src/components/about/About.tsx
+++ frontend/src/components/about/About.tsx
@@ -27,6 +27,19 @@ const About = () => {
       <p>Unsubscribe from channels that are inactive or overactive.</p>
       <p>Delete channels you don&apos;t use anymore.</p>
       <p>If you are being spammed, unsubscribe.</p>
+      <h3>Channel Examples</h3>
+      <h4>Video Games</h4>
+      <p>Game time. Join the call and see you in game.</p>
+      <h4>Coffee Shop Work Hour</h4>
+      <p>Time to focus. Join me at the corner café!</p>
+      <h4>Park Hang</h4>
+      <p>Meet me at the park! I have my speaker and a frisbee. Bring snacks!</p>
+      <h4>Gym Time</h4>
+      <p>You know what time it is. See you at the gym.</p>
+      <h4>Pizza Night</h4>
+      <p>I&apos;m making a bunch of pizzas right now. Come help me eat them!</p>
+      <h4>Free Concert</h4>
+      <p>Free music in the park right now!</p>
       <h3>Frequently Asked Questions</h3>
       <p>
         <strong>What happens when I press the on button?</strong>
