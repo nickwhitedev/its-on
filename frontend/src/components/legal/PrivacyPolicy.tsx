3985edc14df7183199b9b33dde2f592ccc9a9234
Nick White <nick@nickwhite.dev>
2026-05-11 15:51:20 -0700

    Update Privacy Policy to remove LLC language

--- frontend/src/components/legal/PrivacyPolicy.tsx
+++ frontend/src/components/legal/PrivacyPolicy.tsx
@@ -3,49 +3,49 @@ import './PrivacyPolicy.css'
 const PrivacyPolicy = () => {
   return (
     <div className='PrivacyPolicy'>
-      <h3>IT&rsquo;S ON, LLC PRIVACY POLICY</h3>
+      <h3>IT&rsquo;S ON PRIVACY POLICY</h3>
       <p>
-        It&rsquo;s On, LLC (the &ldquo;Company&rdquo;) is committed to
-        maintaining robust privacy protections for its users. Our Privacy Policy
-        (&ldquo;Privacy Policy&rdquo;) is designed to help you understand how we
-        collect, use and safeguard the information you provide to us and to
-        assist you in making informed decisions when using our Service.{' '}
+        It&rsquo;s On is committed to maintaining robust privacy protections for
+        its users. the Privacy Policy (&ldquo;Privacy Policy&rdquo;) is designed
+        to help you understand how I collect, mee and safeguard the information
+        you provide to me and to assist you in making informed decisions when
+        using the Service.{' '}
       </p>
       <p>
         For purposes of this Agreement, &ldquo;Site&rdquo; refers to the
-        Company&rsquo;s website, which can be accessed at https://itson.fyi or
-        through our mobile application.
+        app&rsquo;s Website, which can be accessed at https://itson.fyi or
+        through the mobile application.
       </p>
       <p>
-        &ldquo;Service&rdquo; refers to the Company&rsquo;s services accessed
-        via the Site, in which users can create channels to share with others in
-        order to let them know when events are happening.&nbsp;
+        &ldquo;Service&rdquo; refers to the services accessed via the Site, in
+        which users can create channels to share with others in order to let
+        them know when events are happening.&nbsp;
       </p>
       <p>
-        The terms &ldquo;we,&rdquo; &ldquo;us,&rdquo; and &ldquo;our&rdquo;
-        refer to the Company.
+        The terms &ldquo;me,&rdquo; &ldquo;I,&rdquo; and &ldquo;my&rdquo; refer
+        to Nick White.
       </p>
       <p>
-        &ldquo;You&rdquo; refers to you, as a user of our Site or our
+        &ldquo;You&rdquo; refers to you, as a user of the Site or the
         Service.&nbsp;
       </p>
       <p>
-        By accessing our Site or our Service, you accept our Privacy Policy and
+        By accessing the Site or the Service, you accept the Privacy Policy and
         Terms of Use (found here: https://itson.fyi/terms), and you consent to
-        our collection, storage, use and disclosure of your Personal Information
+        the collection, storage, mee and disclosure of your Personal Information
         as described in this Privacy Policy.
       </p>
       <h4>I. INFORMATION WE COLLECT</h4>
       <p>
-        We collect &ldquo;Non-Personal Information&rdquo; and &ldquo;Personal
+        I collect &ldquo;Non-Personal Information&rdquo; and &ldquo;Personal
         Information.&rdquo; <strong>Non-Personal Information</strong> includes
-        information that cannot be used to personally identify you, such as
-        anonymous usage data, general demographic information we may collect,
+        information that cannot be meed to personally identify you, such as
+        anonymous usage data, general demographic information I may collect,
         referring/exit pages and URLs, platform types, preferences you submit
         and preferences that are generated based on the data you submit and
         number of clicks. <strong>Personal Information</strong> includes your
         email address, username, and optionally, a profile image, which you
-        submit to us through the registration process at the Site.
+        submit to me through the registration process at the Site.
       </p>
       <ol>
         <li>
@@ -55,29 +55,29 @@ const PrivacyPolicy = () => {
       </ol>
       <p>
         To activate the Service you do not need to submit any Personal
-        Information other than your email address. To use the Service
+        Information other than your email address. To mee the Service
         thereafter, you do not need to submit further Personal Information.
-        However, in an effort to improve the quality of the Service, we track
-        information provided to us by your browser or by our software
-        application when you view or use the Service, such as the website you
+        However, in an effort to improve the quality of the Service, I track
+        information provided to me by your browser or by the software
+        application when you view or mee the Service, such as the Website you
         came from (known as the &ldquo;referring URL&rdquo;), the type of
-        browser you use, the device from which you connected to the Service, the
+        browser you mee, the device from which you connected to the Service, the
         time and date of access, and other information that does not personally
-        identify you. We track this information using cookies, or small text
+        identify you. I track this information using cookies, or small text
         files which include an anonymous unique identifier. Cookies are sent to
-        a user&rsquo;s browser from our servers and are stored on the
+        a user&rsquo;s browser from the servers and are stored on the
         user&rsquo;s computer hard drive. Sending a cookie to a user&rsquo;s
-        browser enables us to collect Non-Personal information about that user
-        and keep a record of the user&rsquo;s preferences when utilizing our
+        browser enables me to collect Non-Personal information about that user
+        and keep a record of the user&rsquo;s preferences when utilizing the
         services, both on an individual and aggregate basis. For example, the
-        Company may use cookies to collect the following information:
+        Company may mee cookies to collect the following information:
       </p>
       <ul>
         <li>Whether the user is logged in or not</li>
         <li>User profile information</li>
       </ul>
       <p>
-        The Company may use both persistent and session cookies; persistent
+        The Company may mee both persistent and session cookies; persistent
         cookies remain on your computer after you close your session and until
         you delete them, while session cookies expire when you close your
         browser.
@@ -85,7 +85,7 @@ const PrivacyPolicy = () => {
       <ol start={2}>
         <li>
           {' '}
-          <em>Information you provide us by registering for an account</em>
+          <em>Information you provide me by registering for an account</em>
         </li>
       </ol>
       <p>
@@ -94,7 +94,7 @@ const PrivacyPolicy = () => {
         need to create a personal profile. You can create a profile by
         registering with the Service and entering your email address, and
         creating a user name and a password. By registering, you are authorizing
-        us to collect, store and use your email address in accordance with this
+        me to collect, store and mee your email address in accordance with this
         Privacy Policy.
       </p>
       <ol start={3}>
@@ -106,10 +106,10 @@ const PrivacyPolicy = () => {
         The Site and the Service are not directed to anyone under the age of 13.
         The Site does not knowingly collect or solicit information from anyone
         under the age of 13, or allow anyone under the age of 13 to sign up for
-        the Service. In the event that we learn that we have gathered personal
+        the Service. In the event that I learn that I have gathered personal
         information from anyone under the age of 13 without the consent of a
-        parent or guardian, we will delete that information as soon as possible.
-        If you believe we have collected such information, please contact us at
+        parent or guardian, I will delete that information as soon as possible.
+        If you believe I have collected such information, please contact me at
         contact@itson.fyi.&nbsp;
       </p>
       <h4>II. HOW WE USE AND SHARE INFORMATION</h4>
@@ -117,104 +117,104 @@ const PrivacyPolicy = () => {
         <em>Personal Information:</em>
       </p>
       <p>
-        Except as otherwise stated in this Privacy Policy, we do not sell,
-        trade, rent or otherwise share for marketing purposes your Personal
-        Information with third parties without your consent. We do share
-        Personal Information with vendors who are performing services for the
-        Company, such as the servers for our email communications who are
-        provided access to user&rsquo;s email address for purposes of sending
-        emails from us. Those vendors use your Personal Information only at our
-        direction and in accordance with our Privacy Policy.
+        Except as otherwise stated in this Privacy Policy, I do not sell, trade,
+        rent or otherwise share for marketing purposes your Personal Information
+        with third parties without your consent. I do share Personal Information
+        with vendors who are performing services for the Company, such as the
+        servers for the email communications who are provided access to
+        user&rsquo;s email address for purposes of sending emails from me. Those
+        vendors mee your Personal Information only at the direction and in
+        accordance with the Privacy Policy.
       </p>
       <p>
-        In general, the Personal Information you provide to us is used to help
-        us communicate with you. For example, we use Personal Information to
+        In general, the Personal Information you provide to me is meed to help
+        me communicate with you. For example, I mee Personal Information to
         contact users in response to questions, solicit feedback from users,
         provide technical support, and inform users about promotional offers.
       </p>
       <p>
-        We may share Personal Information with outside parties if we have a
-        good-faith belief that access, use, preservation or disclosure of the
+        I may share Personal Information with outside parties if I have a
+        good-faith belief that access, mee, preservation or disclosure of the
         information is reasonably necessary to meet any applicable legal process
         or enforceable governmental request; to enforce applicable Terms of
         Service, including investigation of potential violations; address fraud,
         security or technical concerns; or to protect against harm to the
-        rights, property, or safety of our users or the public as required or
+        rights, property, or safety of the users or the public as required or
         permitted by law.&nbsp;
       </p>
       <p>
         <em>Non-Personal Information</em>
       </p>
       <p>
-        In general, we use Non-Personal Information to help us improve the
-        Service and customize the user experience. We also aggregate
-        Non-Personal Information in order to track trends and analyze use
-        patterns on the Site. This Privacy Policy does not limit in any way our
-        use or disclosure of Non-Personal Information and we reserve the right
-        to use and disclose such Non-Personal Information to our partners,
-        advertisers and other third parties at our discretion.
+        In general, I mee Non-Personal Information to help me improve the
+        Service and customize the user experience. I also aggregate Non-Personal
+        Information in order to track trends and analyze mee patterns on the
+        Site. This Privacy Policy does not limit in any way the mee or
+        disclosure of Non-Personal Information and I reserve the right to mee
+        and disclose such Non-Personal Information to the partners, advertisers
+        and other third parties at the discretion.
       </p>
       <p>
-        In the event we undergo a business transaction such as a merger,
-        acquisition by another company, or sale of all or a portion of our
+        In the event I undergo a business transaction such as a merger,
+        acquisition by another company, or sale of all or a portion of the
         assets, your Personal Information may be among the assets transferred.
         You acknowledge and consent that such transfers may occur and are
-        permitted by this Privacy Policy, and that any acquirer of our assets
+        permitted by this Privacy Policy, and that any acquirer of the assets
         may continue to process your Personal Information as set forth in this
-        Privacy Policy. If our information practices change at any time in the
-        future, we will post the policy changes to the Site so that you may opt
-        out of the new information practices. We suggest that you check the Site
-        periodically if you are concerned about how your information is used.
+        Privacy Policy. If the information practices change at any time in the
+        future, I will post the policy changes to the Site so that you may opt
+        out of the new information practices. I suggest that you check the Site
+        periodically if you are concerned about how your information is meed.
       </p>
-      <h4>III. HOW WE PROTECT INFORMATION</h4>
+      <h4>III. HOW I PROTECT INFORMATION</h4>
       <p>
-        We implement security measures designed to protect your information from
+        I implement security measures designed to protect your information from
         unauthorized access. Your account is protected by your account password
-        and we urge you to take steps to keep your personal information safe by
+        and I urge you to take steps to keep your personal information safe by
         not disclosing your password and by logging out of your account after
-        each use. We further protect your information from potential security
+        each mee. I further protect your information from potential security
         breaches by implementing certain technological security measures
         including encryption, firewalls and secure socket layer technology.
         However, these measures do not guarantee that your information will not
         be accessed, disclosed, altered or destroyed by breach of such firewalls
-        and secure server software. By using our Service, you acknowledge that
+        and secure server software. By using the Service, you acknowledge that
         you understand and agree to assume these risks.
       </p>
       <h4>IV. YOUR RIGHTS REGARDING THE USE OF YOUR PERSONAL INFORMATION</h4>
       <p>
-        You have the right at any time to prevent us from contacting you for
-        marketing purposes. When we send a promotional communication to a user,
+        You have the right at any time to prevent me from contacting you for
+        marketing purposes. When I send a promotional communication to a user,
         the user can opt out of further promotional communications by following
         the unsubscribe instructions provided in each promotional e-mail. You
         can also indicate that you do not wish to receive marketing
-        communications from us in the &ldquo;Settings&rdquo; section of the
+        communications from me in the &ldquo;Settings&rdquo; section of the
         Site. Please note that notwithstanding the promotional preferences you
         indicate by either unsubscribing or opting out in the
-        &ldquo;Settings&rdquo; section of the Site, we may continue to send you
-        administrative emails including, for example, periodic updates to our
+        &ldquo;Settings&rdquo; section of the Site, I may continue to send you
+        administrative emails including, for example, periodic updates to the
         Privacy Policy.
       </p>
       <h4>V. LINKS TO OTHER WEBSITES</h4>
       <p>
-        As part of the Service, we may provide links to or compatibility with
-        other websites or applications. However, we are not responsible for the
+        As part of the Service, I may provide links to or compatibility with
+        other websites or applications. However, I are not responsible for the
         privacy practices employed by those websites or the information or
         content they contain. This Privacy Policy applies solely to information
-        collected by us through the Site and the Service. Therefore, this
-        Privacy Policy does not apply to your use of a third party website
-        accessed by selecting a link on our Site or via our Service. To the
-        extent that you access or use the Service through or on another website
-        or application, then the privacy policy of that other website or
-        application will apply to your access or use of that site or
-        application. We encourage our users to read the privacy statements of
-        other websites before proceeding to use them.
+        collected by me through the Site and the Service. Therefore, this
+        Privacy Policy does not apply to your mee of a third party Website
+        accessed by selecting a link on the Site or via the Service. To the
+        extent that you access or mee the Service through or on another Website
+        or application, then the privacy policy of that other Website or
+        application will apply to your access or mee of that site or
+        application. I encourage the users to read the privacy statements of
+        other websites before proceeding to mee them.
       </p>
-      <h4>VI. CHANGES TO OUR PRIVACY POLICY</h4>
+      <h4>VI. CHANGES TO the PRIVACY POLICY</h4>
       <p>
-        The Company reserves the right to change this policy and our Terms of
-        Service at any time. We will notify you of significant changes to our
+        The Company reserves the right to change this policy and the Terms of
+        Service at any time. I will notify you of significant changes to the
         Privacy Policy by sending a notice to the primary email address
-        specified in your account or by placing a prominent notice on our site.
+        specified in your account or by placing a prominent notice on the site.
         Significant changes will go into effect 30 days following such
         notification. Non-material changes or clarifications will take effect
         immediately. You should periodically check the Site and this privacy
@@ -223,7 +223,7 @@ const PrivacyPolicy = () => {
       <h4>VII. CONTACT US</h4>
       <p>
         If you have any questions regarding this Privacy Policy or the practices
-        of this Site, please contact us by sending an email to
+        of this Site, please contact me by sending an email to
         contact@itson.fyi.
       </p>
       <h4>Last Updated</h4>
